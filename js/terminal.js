da116fd0399c1d97bc0cd3f874c006f35897212f
Paweł Płóciennik <paplociennik@gmail.com>
2023-04-02 19:58:47 +0200

    tab script

--- js/terminal.js
+++ js/terminal.js
@@ -9,14 +9,28 @@ class terminal extends HTMLElement
         this.open_close = this.open_close.bind(this)
         this.change_settings = this.change_settings.bind(this)
         this.send_string = this.send_string.bind(this)
+        this.send_string_input = this.send_string_input.bind(this)
         this.clear_terminal = this.clear_terminal.bind(this)
         this.detect_enter = this.detect_enter.bind(this)
 
-        this.dtr = this.dtr.bind(this)
-        this.rts = this.rts.bind(this)
+        this.read_line_script = this.read_line_script.bind(this)
 
-        this.dtr_on_off = this.dtr_on_off.bind(this)
-        this.rts_on_off = this.rts_on_off.bind(this)
+        this.dtr_on_ms_button_clicked = this.dtr_on_ms_button_clicked.bind(this)
+        this.dtr_off_ms_button_clicked = this.dtr_off_ms_button_clicked.bind(this)
+        this.rts_on_ms_button_clicked = this.rts_on_ms_button_clicked.bind(this)
+        this.rts_off_ms_button_clicked = this.rts_off_ms_button_clicked.bind(this)
+
+        this.dtr_on = this.dtr_on.bind(this)
+        this.rts_on = this.rts_on.bind(this)
+
+        this.dtr_on_off_ms = this.dtr_on_off_ms.bind(this)
+        this.rts_on_off_ms = this.rts_on_off_ms.bind(this)
+
+        this.dtr_checkbox_clicked = this.dtr_checkbox_clicked.bind(this)
+        this.rts_checkbox_clicked = this.rts_checkbox_clicked.bind(this)
+
+        this.active_term_tab = this.active_term_tab.bind(this)
+        this.active_script_tab = this.active_script_tab.bind(this)
     }
 
     async open_close()
@@ -62,14 +76,18 @@ class terminal extends HTMLElement
                 this.shadowRoot.getElementById("clear").disabled = false
                 this.shadowRoot.getElementById("change").disabled = false
 
-                this.shadowRoot.getElementById("dtr_button").disabled = false
-                this.shadowRoot.getElementById("rts_button").disabled = false
+                this.shadowRoot.getElementById("dtr_on_ms_button").disabled = false
+                this.shadowRoot.getElementById("rts_on_ms_button").disabled = false
+//                this.shadowRoot.getElementById("dtr_off_ms_button").disabled = false
+//                this.shadowRoot.getElementById("rts_off_ms_button").disabled = false
 
                 this.shadowRoot.getElementById("dtr_checkbox").disabled = false
                 this.shadowRoot.getElementById("rts_checkbox").disabled = false
 
                 let port_infonfo = this.port.getInfo()
                 console.log(port_infonfo)
+
+                this.read_text=""
                 this.shadowRoot.getElementById("port_info").innerText =
                     "Connected to   device with VID " +
                     port_infonfo.usbVendorId +
@@ -83,6 +101,14 @@ class terminal extends HTMLElement
                         break
                     }
                     this.shadowRoot.getElementById("term_window").value += value
+                    this.read_text += value
+
+                    let newline_index = this.read_text.search("\n")
+                    while(newline_index >= 0) {
+                        this.read_line_script(this.read_text.substring(0, newline_index))
+                        this.read_text = this.read_text.substring(newline_index + 1, this.read_text.length)
+                        newline_index = this.read_text.search("\n")
+                    }
                 }
 
                 await readable_stream_closed.catch(() => {})
@@ -95,8 +121,10 @@ class terminal extends HTMLElement
                 this.shadowRoot.getElementById("change").disabled = true
                 this.shadowRoot.getElementById("port_info").innerText = "Disconnected"
 
-                this.shadowRoot.getElementById("dtr_button").disabled = true
-                this.shadowRoot.getElementById("rts_button").disabled = true
+                this.shadowRoot.getElementById("dtr_on_ms_button").disabled = true
+                this.shadowRoot.getElementById("rts_on_ms_button").disabled = true
+                this.shadowRoot.getElementById("dtr_off_ms_button").disabled = true
+                this.shadowRoot.getElementById("rts_off_ms_button").disabled = true
 
                 this.shadowRoot.getElementById("dtr_checkbox").disabled = true
                 this.shadowRoot.getElementById("rts_checkbox").disabled = true
@@ -111,80 +139,148 @@ class terminal extends HTMLElement
 
     async change_settings()
     {
-      this.hold_port = this.port
-      this.reader.cancel()
-      console.log("changing setting...")
-      console.log("waiting for port to close...")
-      await this.port_promise
-      console.log("port closed, opening with new settings...")
-      this.open_close()
+        this.hold_port = this.port
+        this.reader.cancel()
+        console.log("changing setting...")
+        console.log("waiting for port to close...")
+        await this.port_promise
+        console.log("port closed, opening with new settings...")
+        this.open_close()
     }
 
-    async send_string()
+    async send_string(out_string)
     {
-      let out_string = this.shadowRoot.getElementById("term_input").value
-      this.shadowRoot.getElementById("term_input").value = ""
-
-      const text_encoder = new TextEncoderStream()
-      const writable_stream_closed = text_encoder.readable.pipeTo(this.port.writable)
-      const writer = text_encoder.writable.getWriter()
-
-      await writer.write(out_string+"\n")
-      this.shadowRoot.getElementById("term_window").value += "\n>" + out_string +"\n"
-
-      writer.close()
-      await writable_stream_closed
-    }
-
-    dtr_on(b)
-    {
-        if (b) {
-            this.shadowRoot.getElementById("dtr_checkbox").checked = true
-            this.port.setSignals({ dataTerminalReady: false })
-        } else {
-            this.shadowRoot.getElementById("dtr_checkbox").checked = false
-            this.port.setSignals({ dataTerminalReady: true })
+        if (!this.port_open) {
+            return
         }
+
+        const text_encoder = new TextEncoderStream()
+        const writable_stream_closed = text_encoder.readable.pipeTo(this.port.writable)
+        const writer = text_encoder.writable.getWriter()
+
+        await writer.write(out_string+"\n")
+        this.shadowRoot.getElementById("term_window").value += "\n>" + out_string +"\n"
+
+        writer.close()
+        await writable_stream_closed
+
     }
 
-    async dtr()
+    async send_string_input()
     {
-        let ms = this.shadowRoot.getElementById("dtr_input").value
-        await this.port.setSignals({ dataTerminalReady: false })
-        await new Promise(resolve => setTimeout(resolve, Number(ms)))
-        await this.port.setSignals({ dataTerminalReady: true })
+        let out_string = this.shadowRoot.getElementById("term_input").value
+        this.shadowRoot.getElementById("term_input").value = ""
+
+        await this.send_string(out_string)
+     }
+
+    async dtr_on(b)
+    {
+        if (!this.port_open) {
+            return
+        }
+
+        this.shadowRoot.getElementById("dtr_on_ms_button").disabled = b
+        this.shadowRoot.getElementById("dtr_off_ms_button").disabled = !b
+        this.shadowRoot.getElementById("dtr_checkbox").checked = b
+        await this.port.setSignals({ dataTerminalReady: !b })
     }
 
-    async rts()
+    async rts_on(b)
     {
-        let ms = this.shadowRoot.getElementById("rts_input").value
-        await this.port.setSignals({ requestToSend : false })
-        await new Promise(resolve => setTimeout(resolve, Number(ms)))
-        await this.port.setSignals({ requestToSend: true })
+        if (!this.port_open) {
+            return
+        }
+
+        this.shadowRoot.getElementById("rts_on_ms_button").disabled = b
+        this.shadowRoot.getElementById("rts_off_ms_button").disabled = !b
+        this.shadowRoot.getElementById("rts_checkbox").checked = b
+        await this.port.setSignals({ requestToSend: !b })
     }
 
-    dtr_on_off()
+    async dtr_on_off_ms(ms, b)
     {
-        this.port.setSignals({ dataTerminalReady: ! this.shadowRoot.getElementById("dtr_checkbox").checked })
+        this.dtr_on(b)
+        await new Promise(resolve => setTimeout(resolve, ms))
+        this.dtr_on(!b)
+}
+
+    async rts_on_off_ms(ms, b)
+    {
+        this.rts_on(b)
+        await new Promise(resolve => setTimeout(resolve, ms))
+        this.rts_on(!b)
     }
 
-    rts_on_off()
+    async dtr_on_ms_button_clicked()
     {
-        this.port.setSignals({ requestToSend : ! this.shadowRoot.getElementById("rts_checkbox").checked })
+        await this.dtr_on_off_ms(Number(this.shadowRoot.getElementById("dtr_on_off_ms_input").value), true)
+    }
+
+    async rts_on_ms_button_clicked()
+    {
+        await this.rts_on_off_ms(Number(this.shadowRoot.getElementById("rts_on_off_ms_input").value), true)
+    }
+
+    async dtr_off_ms_button_clicked()
+    {
+        await this.dtr_on_off_ms(Number(this.shadowRoot.getElementById("dtr_on_off_ms_input").value), false)
+    }
+
+    async rts_off_ms_button_clicked()
+    {
+        await this.rts_on_off_ms(Number(this.shadowRoot.getElementById("rts_on_off_ms_input").value), false)
+    }
+
+    dtr_checkbox_clicked()
+    {
+        this.dtr_on(this.shadowRoot.getElementById("dtr_checkbox").checked)
+    }
+
+    rts_checkbox_clicked()
+    {
+        this.rts_on(this.shadowRoot.getElementById("rts_checkbox").checked )
     }
 
     clear_terminal()
     {
-      this.shadowRoot.getElementById("term_window").value = ""
+        this.shadowRoot.getElementById("term_window").value = ""
+//        eval("this.shadowRoot.getElementById(\"term_window\").value = \"\"")
+    }
+
+    read_line_script(line)
+    {
+//        console.log("["+line+"]");
+        eval("let line = \""+line+"\"; "+this.shadowRoot.getElementById("script_window").value)
     }
 
     detect_enter(e)
     {
-      if (e.keyCode == 13) {
-        e.preventDefault()
-        this.send_string()
-      }
-      return
+        if (!this.port_open) {
+            return
+        }
+
+        if (e.keyCode == 13) {
+            e.preventDefault()
+            this.send_string_input()
+        }
+        return
+    }
+
+    active_term_tab()
+    {
+        this.shadowRoot.getElementById("term_tab").style.backgroundColor = "white"
+        this.shadowRoot.getElementById("script_tab").style.backgroundColor = "grey"
+        this.shadowRoot.getElementById("term_tab_div").style.display = "block"
+        this.shadowRoot.getElementById("script_tab_div").style.display = "none"
+    }
+
+    active_script_tab()
+    {
+        this.shadowRoot.getElementById("term_tab").style.backgroundColor = "grey"
+        this.shadowRoot.getElementById("script_tab").style.backgroundColor = "white"
+        this.shadowRoot.getElementById("term_tab_div").style.display = "none"
+        this.shadowRoot.getElementById("script_tab_div").style.display = "block"
     }
 
     connectedCallback()
@@ -195,11 +291,16 @@ class terminal extends HTMLElement
                 * {
                 box-sizing: border-box
             }
+            .tabname {
+                border: none;
+            }
 
-            body {
-                width: 100%;
-                margin: 0px;
-                padding: 0px;
+            #term_tab {
+                background-color: white;
+            }
+
+            #script_tab {
+                background-color: grey;
             }
 
             #term_window_div {
@@ -214,6 +315,24 @@ class terminal extends HTMLElement
                 height: 600px;
                 margin: 0px;
                 padding: 0px;
+            }
+
+            #script_window_div {
+                width: 100%;
+                margin: 0px;
+                margin: 0px;
+                padding: 0px;
+            }
+
+            #script_window {
+                width: 100%;
+                height: 600px;
+                margin: 0px;
+                padding: 0px;
+            }
+
+            #script_tab_div {
+                display: none;
             }
 
             .flex_row {
@@ -231,9 +350,15 @@ class terminal extends HTMLElement
             .number_input {
                 width: 60px;
             }
-        `
+           `
 
         const html = `
+        <div>
+            <button class="tabname" id="term_tab">Terminal</button>
+            <button class="tabname" id="script_tab">Script</button>
+        </div>
+
+        <div id="term_tab_div">
             <div id="term_window_div">
                 <textarea id="term_window" readonly></textarea>
             </div>
@@ -256,12 +381,14 @@ class terminal extends HTMLElement
                 </div>
             </div>
             <div class="flex_row">
-                <input class="number_input" type="number" min="1" step="1" value="1000" id="dtr_input"></input>
-                <button id="dtr_button" disabled>DTR</button>
+                <input class="number_input" type="number" min="1" step="1" value="100" id="dtr_on_off_ms_input"></input>
+                <button id="dtr_on_ms_button" disabled>DTR on ms</button>
+                <button id="dtr_off_ms_button" disabled>DTR Off ms</button>
             </div>
             <div class="flex_row">
-                <input class="number_input" type="number" min="1" step="1" value="1000" id="rts_input"></input>
-                <button id="rts_button" disabled>RTS</button>
+                <input class="number_input" type="number" min="1" step="1" value="100" id="rts_on_off_ms_input"></input>
+                <button id="rts_on_ms_button" disabled>RTS On ms</button>
+                <button id="rts_off_ms_button" disabled>RTS Off ms</button>
             </div>
             <div class="flex_row">
                 <input type="checkbox" id="dtr_checkbox" disabled>DTR</input>
@@ -272,9 +399,15 @@ class terminal extends HTMLElement
             <div class="flex_row">
                 <span id="port_info">Disconnected</span>
             </div>
+        </div>
+        <div id="script_tab_div">
+            <div id="script_window_div">
+                <textarea id="script_window">
+                </textarea>
+            </div>
+        </div>
 
             `
-
         this.attachShadow({ mode: 'open' })
         this.shadowRoot.innerHTML = `
             <style>
@@ -291,15 +424,25 @@ class terminal extends HTMLElement
 
             this.shadowRoot.getElementById("change").addEventListener("click", this.change_settings)
             this.shadowRoot.getElementById("clear").addEventListener("click", this.clear_terminal)
-            this.shadowRoot.getElementById("send").addEventListener("click", this.send_string)
+            this.shadowRoot.getElementById("send").addEventListener("click", this.send_string_input)
             this.shadowRoot.getElementById("term_input").addEventListener("keydown", this.detect_enter)
 
-            this.shadowRoot.getElementById("dtr_button").addEventListener("click", this.dtr)
-            this.shadowRoot.getElementById("rts_button").addEventListener("click", this.rts)
+            this.shadowRoot.getElementById("dtr_on_ms_button").addEventListener("click", this.dtr_on_ms_button_clicked)
+            this.shadowRoot.getElementById("dtr_off_ms_button").addEventListener("click", this.dtr_off_ms_button_clicked)
+            this.shadowRoot.getElementById("rts_on_ms_button").addEventListener("click", this.rts_on_ms_button_clicked)
+            this.shadowRoot.getElementById("rts_off_ms_button").addEventListener("click", this.rts_off_ms_button_clicked)
 
-            this.shadowRoot.getElementById("dtr_checkbox").addEventListener("click", this.dtr_on_off)
-            this.shadowRoot.getElementById("rts_checkbox").addEventListener("click", this.rts_on_off)
+            this.shadowRoot.getElementById("dtr_checkbox").addEventListener("click", this.dtr_checkbox_clicked)
+            this.shadowRoot.getElementById("rts_checkbox").addEventListener("click", this.rts_checkbox_clicked)
 
+            this.shadowRoot.getElementById("term_tab").addEventListener("click", this.active_term_tab)
+            this.shadowRoot.getElementById("script_tab").addEventListener("click", this.active_script_tab)
+            this.shadowRoot.getElementById("script_window").value =
+                "console.log(\"[script] [\"+line+\"]\")\n"+
+                "if (line == \"qwerty\") {\n"+
+                "    console.log(\"[script send_line] [ytrewq]\")\n"+
+                "    this.send_string(\"ytrewq\")\n"+
+                "}\n"
             this.clear_terminal()
 
             const params = new Proxy(new URLSearchParams(window.location.search), {
